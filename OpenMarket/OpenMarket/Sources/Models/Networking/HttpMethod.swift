be3c52fe3aaeaaf5422e37e4db5da0ac6ed0ec39
junho <junhoL1046@gmail.com>
2023-03-30 22:05:58 +0900

    feat: HttpMethod 구현

---
+++ OpenMarket/OpenMarket/Sources/Models/Networking/HttpMethod.swift
@@ -0,0 +1,19 @@
+enum HttpMethod {
+    case get
+    case post
+    case patch
+    case delete
+
+    var text: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .post:
+            return "POST"
+        case .patch:
+            return "PATCH"
+        case .delete:
+            return "DELETE"
+        }
+    }
+}
