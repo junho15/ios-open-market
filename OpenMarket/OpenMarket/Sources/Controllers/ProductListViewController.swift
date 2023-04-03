513a91e5790a5de7b943183f0c2a9edfdaac6882
junho <junhoL1046@gmail.com>
2023-04-03 15:56:04 +0900

    feat: ProductListViewController 추가 및 스토리보드에서 목록 화면 UI 구현

---
+++ OpenMarket/OpenMarket/Sources/Controllers/ProductListViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class ProductListViewController: UICollectionViewController {
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
