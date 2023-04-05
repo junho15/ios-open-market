4276f84e6644a3c0168095c17d52748d013d1718
junho <junhoL1046@gmail.com>
2023-04-05 20:38:23 +0900

    feat: URLSessionProtocol에 메서드 요구사항 추가

--- OpenMarket/OpenMarket/Sources/Networking/URLSessionProtocol.swift
+++ OpenMarket/OpenMarket/Sources/Networking/URLSessionProtocol.swift
@@ -2,6 +2,7 @@ import Foundation
 
 protocol URLSessionProtocol {
     func execute(request: Requestable, completion: @escaping (Result<Data, Error>) -> Void)
+    func execute(url: URL, completion: @escaping (Result<Data, Error>) -> Void)
 }
 
 extension URLSession: URLSessionProtocol {
@@ -12,6 +13,25 @@ extension URLSession: URLSessionProtocol {
         }
 
         self.dataTask(with: urlRequest) { data, response, error in
+            if let error {
+                completion(.failure(OpenMarketError.networkError(error: error)))
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                completion(.failure(OpenMarketError.badStatus))
+                return
+            }
+            guard let data else {
+                completion(.failure(OpenMarketError.emptyData))
+                return
+            }
+            completion(.success(data))
+        }.resume()
+    }
+
+    func execute(url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        self.dataTask(with: url) { data, response, error in
             if let error {
                 completion(.failure(OpenMarketError.networkError(error: error)))
                 return
--- OpenMarket/OpenMarketTests/StubURLSession.swift
+++ OpenMarket/OpenMarketTests/StubURLSession.swift
@@ -31,4 +31,24 @@ final class StubURLSession: URLSessionProtocol {
             completion(.success(data))
         }
     }
+
+    func execute(url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + delay) { [weak self] in
+            guard let self else { return }
+            if let error {
+                completion(.failure(OpenMarketError.networkError(error: error)))
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                completion(.failure(OpenMarketError.badStatus))
+                return
+            }
+            guard let data else {
+                completion(.failure(OpenMarketError.emptyData))
+                return
+            }
+            completion(.success(data))
+        }
+    }
 }
