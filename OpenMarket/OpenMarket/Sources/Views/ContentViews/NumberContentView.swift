94ae940a5c12340c4ffc846c6f3a76525fb2e054
junho <junhoL1046@gmail.com>
2023-04-12 19:09:16 +0900

    feat: NumberContentView, NumberSegmentContentView 구현

--- OpenMarket/OpenMarket/Sources/Views/ContentViews/NumberContentView.swift
+++ OpenMarket/OpenMarket/Sources/Views/ContentViews/NumberContentView.swift
@@ -6,6 +6,10 @@ class NumberContentView: UIView, UIContentView {
             configure(configuration)
         }
     }
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 0, height: 44)
+    }
+    private let numberTextField = NumberTextField()
 
     init(_ configuration: UIContentConfiguration) {
         self.configuration = configuration
@@ -18,29 +22,87 @@ class NumberContentView: UIView, UIContentView {
     }
 
     func configure(_ configuration: UIContentConfiguration) {
-
+        if let configuration = configuration as? IntNumberConfiguration {
+            numberTextField.numberType = configuration.keyboardType
+            numberTextField.setNumericValue(configuration.number)
+        } else if let configuration = configuration as? DoubleNumberConfiguration {
+            numberTextField.numberType = configuration.numberType
+            numberTextField.setNumericValue(configuration.number)
+        }
     }
 
     private func configureSubviews() {
+        numberTextField.font = UIFont.preferredFont(forTextStyle: .body)
+        numberTextField.adjustsFontForContentSizeCategory = true
+        numberTextField.clearButtonMode = .whileEditing
+        numberTextField.addAction(
+            UIAction(handler: { [weak self] _ in
+                guard let self else { return }
+                if let configuration = configuration as? IntNumberConfiguration {
+                    configuration.onChange?(numberTextField.numericValue() ?? 0)
+                } else if let configuration = configuration as? DoubleNumberConfiguration {
+                    configuration.onChange?(numberTextField.numericValue() ?? 0)
+                }
+            }),
+            for: .editingChanged
+        )
+
+        numberTextField.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(numberTextField)
+
+        let spacing = Constants.layoutSpacing
+        NSLayoutConstraint.activate([
+            numberTextField.topAnchor.constraint(equalTo: topAnchor),
+            numberTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: spacing),
+            numberTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: spacing),
+            numberTextField.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
 
     }
 }
 
 extension NumberContentView {
-    struct Configuration: UIContentConfiguration {
+    private enum Constants {
+        static let layoutSpacing = CGFloat(10)
+    }
+}
+
+extension NumberContentView {
+    struct IntNumberConfiguration: UIContentConfiguration {
+        var number: Int?
+        var keyboardType: NumberTextField.NumberType = .int
+        var onChange: ((Int) -> Void)?
 
         func makeContentView() -> UIView & UIContentView {
             return NumberContentView(self)
         }
 
-        func updated(for state: UIConfigurationState) -> NumberContentView.Configuration {
+        func updated(for state: UIConfigurationState) -> NumberContentView.IntNumberConfiguration {
+            return self
+        }
+    }
+
+    struct DoubleNumberConfiguration: UIContentConfiguration {
+        var number: Double?
+        var numberType: NumberTextField.NumberType = .double
+        var onChange: ((Double) -> Void)?
+
+        func makeContentView() -> UIView & UIContentView {
+            return NumberContentView(self)
+        }
+
+        func updated(for state: UIConfigurationState) -> NumberContentView.DoubleNumberConfiguration {
             return self
         }
     }
 }
 
 extension UICollectionViewCell {
-    func numberConfiguration() -> NumberContentView.Configuration {
-        return NumberContentView.Configuration()
+    func intNumberConfiguration() -> NumberContentView.IntNumberConfiguration {
+        return NumberContentView.IntNumberConfiguration()
+    }
+
+    func doubleNumberConfiguration() -> NumberContentView.DoubleNumberConfiguration {
+        return NumberContentView.DoubleNumberConfiguration()
     }
 }
--- OpenMarket/OpenMarket/Sources/Views/ContentViews/NumberSegmentContentView.swift
+++ OpenMarket/OpenMarket/Sources/Views/ContentViews/NumberSegmentContentView.swift
@@ -6,6 +6,13 @@ class NumberSegmentContentView: UIView, UIContentView {
             configure(configuration)
         }
     }
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 0, height: 44)
+    }
+    private let stackView = UIStackView()
+    private let numberTextField = NumberTextField()
+    private let segmentedControl = UISegmentedControl()
+    private var segmentsTitle = [String]()
 
     init(_ configuration: UIContentConfiguration) {
         self.configuration = configuration
@@ -18,16 +25,77 @@ class NumberSegmentContentView: UIView, UIContentView {
     }
 
     func configure(_ configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else { return }
+        numberTextField.numberType = configuration.numberType
+        numberTextField.setNumericValue(configuration.number)
 
+        if segmentsTitle != configuration.segmentsTitle {
+            segmentedControl.removeAllSegments()
+            configuration.segmentsTitle.enumerated().forEach {
+                segmentedControl.insertSegment(withTitle: $0.element, at: $0.offset, animated: false)
+            }
+            segmentsTitle = configuration.segmentsTitle
+        }
+        guard configuration.selectedSegmentIndex < segmentedControl.numberOfSegments else { return }
+        segmentedControl.selectedSegmentIndex = configuration.selectedSegmentIndex
     }
 
     private func configureSubviews() {
+        numberTextField.font = UIFont.preferredFont(forTextStyle: .body)
+        numberTextField.adjustsFontForContentSizeCategory = true
+        numberTextField.clearButtonMode = .whileEditing
+        numberTextField.addAction(
+            UIAction(handler: { [weak self] _ in
+                guard let self,
+                      let configuration = configuration as? Configuration else { return }
+                    configuration.onChangeNumber?(numberTextField.numericValue() ?? 0)
+            }),
+            for: .editingChanged
+        )
 
+        segmentedControl.addAction(
+            UIAction(handler: { [weak self] _ in
+                guard let self,
+                      let configuration = configuration as? Configuration else { return }
+                configuration.onChangeSegment?(segmentedControl.selectedSegmentIndex)
+            }),
+            for: .valueChanged
+        )
+
+        stackView.axis = .horizontal
+        stackView.spacing = Constants.layoutSpacing
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        [numberTextField, segmentedControl].forEach(stackView.addArrangedSubview)
+        addSubview(stackView)
+
+        let spacing = Constants.layoutSpacing
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: spacing),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: spacing),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        segmentedControl.setContentHuggingPriority(.required, for: .horizontal)
+    }
+}
+
+extension NumberSegmentContentView {
+    private enum Constants {
+        static let layoutSpacing = CGFloat(10)
     }
 }
 
 extension NumberSegmentContentView {
     struct Configuration: UIContentConfiguration {
+        var number: Double?
+        var segmentsTitle: [String] = []
+        var selectedSegmentIndex: Int = 0
+        var numberType: NumberTextField.NumberType = .double
+        var onChangeNumber: ((Double) -> Void)?
+        var onChangeSegment: ((Int) -> Void)?
 
         func makeContentView() -> UIView & UIContentView {
             return NumberSegmentContentView(self)
