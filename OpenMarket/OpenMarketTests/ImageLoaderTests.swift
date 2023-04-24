2547e79b113b12ccad090bfca650169d9aec53f8
junho <junhoL1046@gmail.com>
2023-04-24 17:40:56 +0900

    test: async 메서드를 사용하도록 테스트 케이스 수정

--- OpenMarket/OpenMarketTests/ImageLoaderTests.swift
+++ OpenMarket/OpenMarketTests/ImageLoaderTests.swift
@@ -20,7 +20,7 @@ final class ImageLoaderTests: XCTestCase {
         try super.tearDownWithError()
     }
 
-    func test_정상적인상황에서_imageLoad_호출시_sucess를_반환하는지() {
+    func test_정상적인상황에서_imageLoad_호출시_sucess를_반환하는지() async {
         // given
         let imageURL = URL(string: "https://test.com/image.jpg")!
         spyURLSession.data = UIImage.add.jpegData(compressionQuality: 1.0)!
@@ -28,28 +28,15 @@ final class ImageLoaderTests: XCTestCase {
                                                  statusCode: 200,
                                                  httpVersion: nil,
                                                  headerFields: nil)
-        let expectation = self.expectation(description: "ImageLoad Complete")
 
         // when
-        sut.loadImage(from: imageURL) { result in
-            // then
-            switch result {
-            case .success:
-                XCTAssert(true)
-            case .failure(let error):
-                XCTFail(error.localizedDescription)
-            }
-            expectation.fulfill()
-        }
+        let resultImage = try? await sut.loadImage(from: imageURL)
 
-        waitForExpectations(timeout: 1.0) { error in
-            if let error {
-                XCTFail(error.localizedDescription)
-            }
-        }
+        // then
+        XCTAssertNotNil(resultImage)
     }
 
-    func test_imageLoad_호출시_에러가_발생하면_error를_반환하는지() {
+    func test_imageLoad_호출시_에러가_발생하면_error를_반환하는지() async {
         // given
         let imageURL = URL(string: "https://test.com/image.jpg")!
         spyURLSession.data = UIImage.add.jpegData(compressionQuality: 1.0)!
@@ -57,28 +44,18 @@ final class ImageLoaderTests: XCTestCase {
                                                  statusCode: 500,
                                                  httpVersion: nil,
                                                  headerFields: nil)
-        let expectation = self.expectation(description: "ImageLoad Complete")
 
         // when
-        sut.loadImage(from: imageURL) { result in
+        do {
+            _ = try await sut.loadImage(from: imageURL)
+            XCTFail("Should return failure")
+        } catch {
             // then
-            switch result {
-            case .success:
-                XCTFail("Should return failure")
-            case .failure:
-                XCTAssert(true)
-            }
-            expectation.fulfill()
-        }
-
-        waitForExpectations(timeout: 1.0) { error in
-            if let error {
-                XCTFail(error.localizedDescription)
-            }
+            XCTAssertTrue(error is OpenMarketError)
         }
     }
 
-    func test_캐싱된_이미지가_있으면_캐싱된_이미지를_반환하고_네트워크에서_Load하지않는지() {
+    func test_캐싱된_이미지가_있으면_캐싱된_이미지를_반환하고_네트워크에서_Load하지않는지() async throws {
         // given
         let imageURL = URL(string: "https://test.com/image.jpg")!
         spyURLSession.data = UIImage.remove.jpegData(compressionQuality: 1.0)!
@@ -88,29 +65,16 @@ final class ImageLoaderTests: XCTestCase {
                                                  headerFields: nil)
         let cacheImage = UIImage.add
         imageCache.store(CachedImage(url: imageURL, image: cacheImage))
-        let expectation = self.expectation(description: "ImageLoad Complete")
 
         // when
-        sut.loadImage(from: imageURL) { result in
-            // then
-            switch result {
-            case .success(let resultImage):
-                XCTAssertEqual(resultImage, cacheImage)
-                XCTAssertEqual(self.spyURLSession.executeCallCount, 0)
-            case .failure(let error):
-                XCTFail(error.localizedDescription)
-            }
-            expectation.fulfill()
-        }
+        let resultImage = try await sut.loadImage(from: imageURL)
 
-        waitForExpectations(timeout: 1.0) { error in
-            if let error {
-                XCTFail(error.localizedDescription)
-            }
-        }
+        // then
+        XCTAssertEqual(resultImage, cacheImage)
+        XCTAssertEqual(spyURLSession.executeCallCount, 0)
     }
 
-    func test_캐싱된_이미지가_기한이만료되었다면_캐싱된_이미지를_반환하지않고_네트워크에서_Load하는지() {
+    func test_캐싱된_이미지가_기한이만료되었다면_캐싱된_이미지를_반환하지않고_네트워크에서_Load하는지() async throws {
         // given
         let imageURL = URL(string: "https://test.com/image.jpg")!
         spyURLSession.data = UIImage.remove.jpegData(compressionQuality: 1.0)!
@@ -122,29 +86,16 @@ final class ImageLoaderTests: XCTestCase {
         imageCache.store(CachedImage(url: imageURL,
                                      image: cacheImage,
                                      timestamp: Date(timeIntervalSinceNow: TimeInterval(-600))))
-        let expectation = self.expectation(description: "ImageLoad Complete")
 
         // when
-        sut.loadImage(from: imageURL) { result in
-            // then
-            switch result {
-            case .success(let resultImage):
-                XCTAssertNotEqual(resultImage, cacheImage)
-                XCTAssertEqual(self.spyURLSession.executeCallCount, 1)
-            case .failure(let error):
-                XCTFail(error.localizedDescription)
-            }
-            expectation.fulfill()
-        }
+        let resultImage = try await sut.loadImage(from: imageURL)
 
-        waitForExpectations(timeout: 1.0) { error in
-            if let error {
-                XCTFail(error.localizedDescription)
-            }
-        }
+        // then
+        XCTAssertNotEqual(resultImage, cacheImage)
+        XCTAssertEqual(self.spyURLSession.executeCallCount, 1)
     }
 
-    func test_중복해서_이미지요청시_적절하게_처리되는지() {
+    func test_중복해서_이미지요청시_적절하게_처리되는지() async throws {
         // given
         let imageURL = URL(string: "https://test.com/image.jpg")!
         spyURLSession.data = UIImage.add.jpegData(compressionQuality: 1.0)!
@@ -152,37 +103,12 @@ final class ImageLoaderTests: XCTestCase {
                                                  statusCode: 200,
                                                  httpVersion: nil,
                                                  headerFields: nil)
-        let firstExpectation = self.expectation(description: "First ImageLoad Complete")
-        let secondExpectation = self.expectation(description: "Second ImageLoad Complete")
 
         // when
-        var firstImage: UIImage?
-        var secondImage: UIImage?
-        sut.loadImage(from: imageURL) { result in
-            switch result {
-            case .success(let image):
-                firstImage = image
-            case .failure:
-                XCTFail("Should return first Image")
-            }
-            firstExpectation.fulfill()
-        }
-        sut.loadImage(from: imageURL) { result in
-            switch result {
-            case .success(let image):
-                secondImage = image
-            case .failure:
-                XCTFail("Should return second Image")
-            }
-            secondExpectation.fulfill()
-        }
+        let firstImage = try await sut.loadImage(from: imageURL)
+        let secondImage = try await sut.loadImage(from: imageURL)
 
         // then
-        waitForExpectations(timeout: 1.0) { error in
-            if let error {
-                XCTFail(error.localizedDescription)
-            }
-        }
         XCTAssertEqual(firstImage, secondImage)
         XCTAssertEqual(spyURLSession.executeCallCount, 1)
     }
